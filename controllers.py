a2bb667c0bdb51f23b240bf16e9dd50fc792533c
isaac <isw9@case.edu>
2019-12-05 10:19:25 -0500

    increase threshold for leaders

--- controllers.py
+++ controllers.py
@@ -102,7 +102,7 @@ def api_leaders(year, number_players, position):
 
     conn = mysql.connect()
     cursor = conn.cursor()
-    in_clause = "(SELECT player.playerID FROM game, player WHERE game.playerID = player.playerID AND fantasyfootball.player.position = (\'{}\') AND fantasyfootball.game.season = {} GROUP BY player.playerID HAVING COUNT(player.playerID) > 5)".format(position, year)
+    in_clause = "(SELECT player.playerID FROM game, player WHERE game.playerID = player.playerID AND fantasyfootball.player.position = (\'{}\') AND fantasyfootball.game.season = {} GROUP BY player.playerID HAVING COUNT(player.playerID) > 8)".format(position, year)
     statement = "SELECT * FROM game, player WHERE game.playerID = player.playerID AND player.position = (\'{}\') AND game.season = {} AND player.playerID IN {};".format(position, year, in_clause)
     cursor.execute(statement)
 
