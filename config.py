49f9e8a2bf2867ed2e463a7b7ba76291d2142724
isaac <isw9@case.edu>
2019-12-05 00:02:07 -0500

    add credentials

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.urandom(32)
-    MYSQL_DATABASE_USER = 'secret'
-    MYSQL_DATABASE_PASSWORD = 'secret'
-    MYSQL_DATABASE_DB = 'secret'
-    MYSQL_DATABASE_HOST = 'secret'
+    MYSQL_DATABASE_USER = 'admin'
+    MYSQL_DATABASE_PASSWORD =  'PISkuqOD1QqBLm2ben67FuZSmzOLalxnFPD7UFdY'
+    MYSQL_DATABASE_DB = 'fantasyfootball'
+    MYSQL_DATABASE_HOST = 'fantasyfootball.crvtzg1tpa0m.us-east-1.rds.amazonaws.com'
